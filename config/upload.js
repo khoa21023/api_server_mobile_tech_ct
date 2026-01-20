8d725eaa627dcf4f021b6dca38201d4def7a2518
Khoa <khoa7220218@gmail.com>
2026-01-20 14:49:21 +0700

    Fix loi upload bang createRequire

--- config/upload.js
+++ config/upload.js
@@ -1,9 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
-import multerStorageCloudinary from 'multer-storage-cloudinary';
 import multer from 'multer';
 import dotenv from 'dotenv';
 
-const { CloudinaryStorage } = multerStorageCloudinary;
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 dotenv.config();
 
